4a68983947a017d76fcb40b42d884f368581e7ae
ibrahimtrahim <trahimbrahim@gmail.com>
2024-02-04 14:05:12 +0000

    animated cursor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AnimCursor from './component/AnimCursor';
 import Header from './component/Header';
 import Hero from './component/Hero';
 import AllRoutes from './routes/AllRoutes';
@@ -8,6 +9,7 @@ import AllRoutes from './routes/AllRoutes';
 function App() {
   return (
     <div className="lg:flex bg-white-peimary dark:bg-black h-screen lg:justify-center lg:items-center">
+      <AnimCursor/>
       <Header />
       <Hero />
       <AllRoutes />
