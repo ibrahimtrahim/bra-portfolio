cfd69d9909ee18aacff6ec859e27214dd7c0dacc
ibrahimtrahim <trahimbrahim@gmail.com>
2024-04-14 21:57:39 +0000

    navbar changes

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,25 +5,31 @@ import Switcher from './Switcher';
 
 const Header = () => {
 
+  const [activePage, setActivePage] = useState(window.location.pathname);
+
+  const handleNavLinkClick = (to) => {
+    setActivePage(to);
+  };
+
   return (
     <header className="lg:flex bg-white dark:bg-dark-primary text-white p-4 rounded lg:items-center mb-4 lg:mr-2 z-50">
       <nav className="lg:flex lg:flex-row lg:justify-center lg:items-center font-lekton font-medium">
         <ul>
         <Switcher />
-        <NavLink icon={faHome} label="Home" to="/home" />
-        <NavLink icon={faUser} label="About" to="/about" />
-        <NavLink icon={faBriefcase} label="Projects" to="/projects" />
-        <NavLink className='hover:text-blue-400' icon={faEnvelope} label="Contact" to="/contact" />
+        <NavLink icon={faHome} label="Home" to="/home" active={activePage === '/home'} onClick={handleNavLinkClick} />
+        <NavLink icon={faUser} label="About" to="/about" active={activePage === '/about'} onClick={handleNavLinkClick} />
+        <NavLink icon={faBriefcase} label="Projects" to="/projects" active={activePage === '/projects'} onClick={handleNavLinkClick} />
+        <NavLink className='hover:text-blue-400' icon={faEnvelope} label="Contact" to="/contact" active={activePage === '/contact'} onClick={handleNavLinkClick} />
         </ul>
       </nav>
     </header>
   );
 };
 
-const NavLink = ({ icon, label, to }) => {
+const NavLink = ({ icon, label, to, active, onClick }) => {
   return (
     <li className='inline'>
-      <a href={to} className="mx-3 text-black dark:text-white lg:flex lg:flex-col hover:text-blue-400 dark:hover:text-blue-400 hover:duration-200 hover:ease-in-out lg:items-center lg:py-2 lg:my-6">
+      <a href={to} onClick={() => onClick(to)} className={`mx-3 text-black dark:text-white lg:flex lg:flex-col ${active ? 'text-blue-400 dark:text-blue-400' : 'hover:text-blue-400 dark:hover:text-blue-400'} lg:items-center lg:py-2 lg:my-6`}>
         <FontAwesomeIcon icon={icon} className="mr-1 lg:mb-2" />
         <span className='text-xs font-medium uppercase'>{label}</span>
       </a>
